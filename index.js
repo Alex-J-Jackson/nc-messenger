29ae62a59a31aac2c2690d8f07ae47f1d53575d9
Alex-J-Jackson <alexander-jackson@outlook.com>
2022-08-31 15:18:04 +0100

    add socket set-up

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// const port = process.env.port || 8080;
+const cors = require("cors");
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
